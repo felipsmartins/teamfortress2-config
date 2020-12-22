9897ac0f308942bfd84e7f97cfcb2f91c6e5e44b
Martins, F <felipsmartins@gmail.com>
2020-12-22 00:50:08 -0300

    Adicionado melhoria nos scripts - shebang (python); execução do steam depois de preparação de CPU e GPU

--- runtf2.sh
+++ runtf2.sh
@@ -13,3 +13,7 @@ sudo cpupower frequency-set -g performance
 /usr/bin/nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=1'
 # menu GUI: X Screen -> OpenGL Settings -> [Performance] -> Image Setttings 
 /usr/bin/nvidia-settings -a '[screen:0]/OpenGLImageSettings=3'
+
+echo "iniciando steam..."
+export STEAM_RUNTIME_PREFER_HOST_LIBRARIES=0
+steam -applaunch $GAME_ID $LAUNCH_OPTIONS
