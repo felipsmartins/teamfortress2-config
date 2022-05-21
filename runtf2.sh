2ae476723586565ef25b1b5c8ba256bb4c0a502b
Martins, F <felipsmartins@gmail.com>
2022-05-21 00:54:15 -0300

    update launch options

--- runtf2.sh
+++ runtf2.sh
@@ -7,7 +7,8 @@ GAME_ID=440
 # https://docs.mastercomfig.com
 # https://developer.valvesoftware.com/wiki/Command_Line_Options#Steam_.28Windows.29
 # obs.: se deixar no steam client (launch options do game) deveria funcionar tambem
-LAUNCH_OPTIONS="+net_graph 1 -full -console -dxlevel 80 -nojoy -nosteamcontroller -softparticlesdefaultoff -reuse -nohltv -nostartupsound --r_emulate_gl -limitvsconst -gl_enablesamplerobjects -novid"
+
+LAUNCH_OPTIONS="SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0 -full -novid -nojoy -nostartupsound"
 
 sudo cpupower frequency-set -g performance
 /usr/bin/nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=1'
